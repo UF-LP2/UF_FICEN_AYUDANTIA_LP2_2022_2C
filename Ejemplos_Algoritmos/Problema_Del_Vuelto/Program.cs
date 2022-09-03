7f43a16410b05449321f58bc308fa80e3a5c5561
eastanganelli <ezequielstanganelli@gmail.com>
2022-09-03 12:52:22 -0300

    Exchange_Coin_Greedy

--- Ejemplos_Algoritmos/Problema_Del_Vuelto/Program.cs
+++ Ejemplos_Algoritmos/Problema_Del_Vuelto/Program.cs
@@ -3,7 +3,8 @@
 using Greedy_CoinChange;
 
 void main() {
-    // Greedy_CoinChange.Greedy_CoinChange Problema_Greedy = new Greedy_CoinChange.Greedy_CoinChange();
+    Greedy_CoinChange.Greedy_CoinChange Problema_Greedy = new Greedy_CoinChange.Greedy_CoinChange(25, new int[]{10, 5, 2, 1});
+    Problema_Greedy.print_dictionary();
 
 
     
--- Ejemplos_Algoritmos/Problema_Del_Vuelto/coinchange_greedy.cs
+++ Ejemplos_Algoritmos/Problema_Del_Vuelto/coinchange_greedy.cs
@@ -1,15 +1,10 @@
 namespace Greedy_CoinChange {
     public class Greedy_CoinChange {
-        public Dictionary<int, int> CoinExchange {
-            get { return CoinExchange; }
-            private set { CoinExchange = value; }
-        }
-        public int[] Coins {
-            get { return Coins; }
-            private set { Coins = value; }
-            
-        }
-        Greedy_CoinChange(int Exchange) {
+        public Dictionary<int, int> CoinExchange;
+        public int[] Coins;
+        public Greedy_CoinChange( int Exchange, int[] Coins ) {
+            this.Coins = Coins;
+
             // Inicializamos el Diccionario
             this.CoinExchange = new Dictionary<int, int>();
 
@@ -23,17 +18,44 @@ namespace Greedy_CoinChange {
                 return;
             
             int Coin_ = Change_finder(Exchange, 0);
-            if( Coin_ != 0 ) {
-                Coins.
-            }
+            if ( Coin_ != 0 ) {
                 
+                if ( CoinExchange.ContainsKey(Coin_) )
+                    CoinExchange[Coin_] += CoinExchange[Coin_];
+                else
+                    CoinExchange.Add(Coin_, 1);
+
+                Exchange -= Coin_;
+
+            }
             greedy_method(Exchange);
         }
+
+        // Buscar de forma recursiva la moneda
         int Change_finder(int Value, int position) {
             if( ( position + 1 ) == Coins.Length )
                 return 0;
 
-            return Value <= Coins[position] ? Coins[position] : Change_finder(Value, position + 1); 
+            if ( Value >= Coins[position] ) 
+                return Coins[position];
+            
+            return Change_finder(Value, position + 1); 
+        }
+
+        //
+        public void print_dictionary() {
+
+            int count_coins = 0;
+
+            Console.WriteLine("Vuelto:");
+
+            foreach ( KeyValuePair<int, int> Coin_ in CoinExchange ) {
+                Console.WriteLine( "$" + Coin_.Key + " [" + Coin_.Value + "]" );
+
+                count_coins += Coin_.Value;
+            }
+
+            Console.WriteLine( "Cantidad total de monedas: " + count_coins );
         }
     }
 }
