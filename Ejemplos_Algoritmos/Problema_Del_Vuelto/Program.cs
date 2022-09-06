fc60f0c6463cda447089bd1b57f5faa06c44c7c6
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-09-06 05:15:25 +0000

    CoinChange Algorithms Finished

--- Ejemplos_Algoritmos/Problema_Del_Vuelto/Program.cs
+++ Ejemplos_Algoritmos/Problema_Del_Vuelto/Program.cs
@@ -1,13 +1,22 @@
 ﻿// Ejemplo Problema de la moneda Greedy y Dinámico
 
-using Greedy_CoinChange;
-
 void main() {
-    Greedy_CoinChange.Greedy_CoinChange Problema_Greedy = new Greedy_CoinChange.Greedy_CoinChange(25, new int[]{10, 5, 2, 1});
-    Problema_Greedy.print_dictionary();
 
+    int[] AvailableCoins = { 1, 2, 3 };
 
-    
+    {
+        Algorithms.CoinChangeG Problema_Greedy = new Algorithms.CoinChangeG( 5, AvailableCoins );
+        Problema_Greedy.print_dictionary();
+    }
+
+    {
+        Algorithms.CoinChangeDP Problema_DP = new Algorithms.CoinChangeDP( 5, AvailableCoins );
+        Problema_DP.print_dictionary();
+    }
+
+    Console.WriteLine("Press any key to Exit...");
+    Console.ReadKey();
+
 }
 
 main();
