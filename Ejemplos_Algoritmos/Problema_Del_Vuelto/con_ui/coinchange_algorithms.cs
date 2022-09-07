3a366f00a887641ca3d12b1babc06d30758ff821
eastanganelli <ezequielstanganelli@gmail.com>
2022-09-07 09:49:56 -0300

    Changes In GUI App

--- Ejemplos_Algoritmos/Problema_Del_Vuelto/con_ui/coinchange_algorithms.cs
+++ Ejemplos_Algoritmos/Problema_Del_Vuelto/con_ui/coinchange_algorithms.cs
@@ -1,13 +1,9 @@
 namespace Algorithms {
     // G: Greedy
     public class CoinChangeG {
-        Dictionary<int, int> Change_G;
         int[] Coins;
         public CoinChangeG( int Sum, int[] Coins ) {
             this.Coins = Coins;
-
-            // Inicializamos el Diccionario
-            this.Change_G = new Dictionary<int, int>();
 
             // Arrancamos el proceso obtener el vuelto
             this.greedy_method( Sum );
@@ -21,22 +17,11 @@ namespace Algorithms {
             int Coin_ = Change_finder( Sum, 0 );
             if ( Coin_ != 0 ) {
 
-                this.check_dictionary(Coin_);
+                //this.check_dictionary(Coin_);
                 Sum -= Coin_;
 
             }
             greedy_method(Sum);
-        }
-
-        // Verifica si el key se encuentra o no dentro del diccionario, de no estar lo agrega
-        private void check_dictionary( int Coin_ ) {
-
-            if( this.Change_G.ContainsKey(Coin_) ) {
-                this.Change_G[Coin_]++;
-                return;
-            }
-            this.Change_G.Add( Coin_, 1 );
-
         }
 
         // Buscar de forma recursiva la moneda
@@ -50,25 +35,10 @@ namespace Algorithms {
             return Change_finder( Value, position + 1 ); 
         }
 
-        //
-        public void print_dictionary() {
-
-            int count_coins = 0;
-            Console.Write( "Vuelto: " );
-
-            foreach ( KeyValuePair<int, int> Coin_ in Change_G ) {
-                Console.Write( "$" + Coin_.Key + " [" + Coin_.Value + "]" );
-                count_coins += Coin_.Value;
-            }
-
-            Console.Write( " >>> Cantidad total de monedas: " + count_coins );
-        }
     }
 
     // DP: Dynamic Programming
     public class CoinChangeDP {
-        private List<int[]> Change_DP = new List<int[]>();
-        private List<Dictionary<int, int>> Dict_DP = new List<Dictionary<int, int>>();
         private int Count_Options = 0;
         public CoinChangeDP( int Sum, int[] Coins ) {
             
@@ -76,11 +46,6 @@ namespace Algorithms {
 
             // Arrancamos el proceso obtener el vuelto
             this.Count_Options = this.methodDP( Coins, Coins.Length, Sum, Change_Coins, Sum );
-
-            // Transformamos el listado de array a diccionario
-            this.Array_to_Dictionary();
-
-            this.Change_DP.Clear();
 
         }
         private int methodDP( int[] CoinChange_, int n_, int Sum_, int[] Change_, int ctrl_ ) {
@@ -91,7 +56,7 @@ namespace Algorithms {
             }
 
             if ( Sum_ == 0 ) {
-                this.Change_DP.Add(Change_);
+                //this.Change_DP.Add(Change_);
                 return 1;
             }
 
@@ -113,59 +78,5 @@ namespace Algorithms {
 
         }
 
-        // Convertir arrays to diccionario
-        private void Array_to_Dictionary() {
-
-            foreach ( int[] values_ in this.Change_DP ) {
-
-                Dictionary<int, int> aux_ = new Dictionary<int, int>();
-
-                foreach( int node in values_ ) {
-
-                    check_dictionary( node, ref aux_ );
-
-                }
-
-                this.Dict_DP.Add(aux_);
-
-            }
-
-        }
-
-        // Verifica si el key se encuentra o no dentro del diccionario, de no estar lo agrega
-        private void check_dictionary( int Coin_, ref Dictionary<int, int> Dict_aux ) {
-
-            if( Dict_aux.ContainsKey(Coin_) ) {
-                Dict_aux[Coin_]++;
-                return;
-            }
-
-            Dict_aux.Add( Coin_, 1 );
-
-        }
-
-        // Imprimir el lista de posibles vueltos
-        public void print_dictionary() {
-
-            int i = 1;
-
-            foreach ( Dictionary<int, int> node in this.Dict_DP ) {
-
-                int count_coins = 0;
-                Console.Write("Iteration: {0} de {1} :: ", i, this.Count_Options);
-
-                foreach ( KeyValuePair<int, int> value_ in node ) {
-
-                    Console.Write("${0} [{1}] ", value_.Key, value_.Value);
-                    count_coins += value_.Value;
-
-                }
-
-                Console.WriteLine("");
-                i++;
-
-            }
-
-        }
     }
 }
--- Ejemplos_Algoritmos/Problema_Del_Vuelto/con_ui/formAlgorithms.Designer.cs
+++ Ejemplos_Algoritmos/Problema_Del_Vuelto/con_ui/formAlgorithms.Designer.cs
@@ -28,83 +28,117 @@ partial class formAlgorithms
     /// </summary>
     private void InitializeComponent()
     {
-            this.radioGreedy = new System.Windows.Forms.RadioButton();
-            this.radiodynamicProg = new System.Windows.Forms.RadioButton();
-            this.lblAlgo = new System.Windows.Forms.Label();
-            this.txtChange = new System.Windows.Forms.TextBox();
+            this.split_Conenedor = new System.Windows.Forms.SplitContainer();
+            this.lbl_Input = new System.Windows.Forms.Label();
             this.btnRun = new System.Windows.Forms.Button();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.lblAlgo = new System.Windows.Forms.Label();
+            this.radiodynamicProg = new System.Windows.Forms.RadioButton();
+            this.radioGreedy = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.split_Conenedor)).BeginInit();
+            this.split_Conenedor.Panel1.SuspendLayout();
+            this.split_Conenedor.SuspendLayout();
             this.SuspendLayout();
             // 
-            // radioGreedy
+            // split_Conenedor
             // 
-            this.radioGreedy.AutoSize = true;
-            this.radioGreedy.Location = new System.Drawing.Point(12, 50);
-            this.radioGreedy.Name = "radioGreedy";
-            this.radioGreedy.Size = new System.Drawing.Size(53, 19);
-            this.radioGreedy.TabIndex = 0;
-            this.radioGreedy.TabStop = true;
-            this.radioGreedy.Text = "Voráz";
-            this.radioGreedy.UseVisualStyleBackColor = true;
+            this.split_Conenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.split_Conenedor.Location = new System.Drawing.Point(0, 0);
+            this.split_Conenedor.Name = "split_Conenedor";
             // 
-            // radiodynamicProg
+            // split_Conenedor.Panel1
             // 
-            this.radiodynamicProg.AutoSize = true;
-            this.radiodynamicProg.Location = new System.Drawing.Point(12, 84);
-            this.radiodynamicProg.Name = "radiodynamicProg";
-            this.radiodynamicProg.Size = new System.Drawing.Size(153, 19);
-            this.radiodynamicProg.TabIndex = 1;
-            this.radiodynamicProg.TabStop = true;
-            this.radiodynamicProg.Text = "Programación Dinámica";
-            this.radiodynamicProg.UseVisualStyleBackColor = true;
+            this.split_Conenedor.Panel1.Controls.Add(this.lbl_Input);
+            this.split_Conenedor.Panel1.Controls.Add(this.btnRun);
+            this.split_Conenedor.Panel1.Controls.Add(this.txtChange);
+            this.split_Conenedor.Panel1.Controls.Add(this.lblAlgo);
+            this.split_Conenedor.Panel1.Controls.Add(this.radiodynamicProg);
+            this.split_Conenedor.Panel1.Controls.Add(this.radioGreedy);
+            this.split_Conenedor.Size = new System.Drawing.Size(743, 390);
+            this.split_Conenedor.SplitterDistance = 247;
+            this.split_Conenedor.TabIndex = 0;
+            // 
+            // lbl_Input
+            // 
+            this.lbl_Input.AutoSize = true;
+            this.lbl_Input.Location = new System.Drawing.Point(47, 190);
+            this.lbl_Input.Name = "lbl_Input";
+            this.lbl_Input.Size = new System.Drawing.Size(44, 15);
+            this.lbl_Input.TabIndex = 10;
+            this.lbl_Input.Text = "Vuelto:";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(89, 252);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 45);
+            this.btnRun.TabIndex = 9;
+            this.btnRun.Text = "Correr";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Location = new System.Drawing.Point(47, 208);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.Size = new System.Drawing.Size(153, 23);
+            this.txtChange.TabIndex = 8;
             // 
             // lblAlgo
             // 
             this.lblAlgo.AutoSize = true;
-            this.lblAlgo.Location = new System.Drawing.Point(12, 18);
+            this.lblAlgo.Location = new System.Drawing.Point(47, 104);
             this.lblAlgo.Name = "lblAlgo";
             this.lblAlgo.Size = new System.Drawing.Size(117, 15);
-            this.lblAlgo.TabIndex = 2;
+            this.lblAlgo.TabIndex = 7;
             this.lblAlgo.Text = "Algoritmo del Vuelto";
             // 
-            // txtChange
+            // radiodynamicProg
             // 
-            this.txtChange.Location = new System.Drawing.Point(12, 126);
-            this.txtChange.Name = "txtChange";
-            this.txtChange.Size = new System.Drawing.Size(153, 23);
-            this.txtChange.TabIndex = 3;
+            this.radiodynamicProg.AutoSize = true;
+            this.radiodynamicProg.Location = new System.Drawing.Point(47, 147);
+            this.radiodynamicProg.Name = "radiodynamicProg";
+            this.radiodynamicProg.Size = new System.Drawing.Size(153, 19);
+            this.radiodynamicProg.TabIndex = 6;
+            this.radiodynamicProg.TabStop = true;
+            this.radiodynamicProg.Text = "Programación Dinámica";
+            this.radiodynamicProg.UseVisualStyleBackColor = true;
             // 
-            // btnRun
+            // radioGreedy
             // 
-            this.btnRun.Location = new System.Drawing.Point(200, 126);
-            this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(75, 45);
-            this.btnRun.TabIndex = 4;
-            this.btnRun.Text = "Correr";
-            this.btnRun.UseVisualStyleBackColor = true;
-            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            this.radioGreedy.AutoSize = true;
+            this.radioGreedy.Location = new System.Drawing.Point(47, 122);
+            this.radioGreedy.Name = "radioGreedy";
+            this.radioGreedy.Size = new System.Drawing.Size(53, 19);
+            this.radioGreedy.TabIndex = 5;
+            this.radioGreedy.TabStop = true;
+            this.radioGreedy.Text = "Voráz";
+            this.radioGreedy.UseVisualStyleBackColor = true;
             // 
             // formAlgorithms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(296, 194);
-            this.Controls.Add(this.btnRun);
-            this.Controls.Add(this.txtChange);
-            this.Controls.Add(this.lblAlgo);
-            this.Controls.Add(this.radiodynamicProg);
-            this.Controls.Add(this.radioGreedy);
+            this.ClientSize = new System.Drawing.Size(743, 390);
+            this.Controls.Add(this.split_Conenedor);
+            this.MaximumSize = new System.Drawing.Size(759, 429);
+            this.MinimumSize = new System.Drawing.Size(759, 429);
             this.Name = "formAlgorithms";
             this.Text = "Selector de Algoritmo";
+            this.split_Conenedor.Panel1.ResumeLayout(false);
+            this.split_Conenedor.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.split_Conenedor)).EndInit();
+            this.split_Conenedor.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
     }
 
     #endregion
 
-    private RadioButton radioGreedy;
-    private RadioButton radiodynamicProg;
-    private Label lblAlgo;
-    private TextBox txtChange;
+    private SplitContainer split_Conenedor;
+    private Label lbl_Input;
     private Button btnRun;
+    private TextBox txtChange;
+    private Label lblAlgo;
+    private RadioButton radiodynamicProg;
+    private RadioButton radioGreedy;
 }
